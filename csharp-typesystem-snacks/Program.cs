7baf7d9fbec51bf22e4a90330277380504cabe8c
Gaspare <Gas9220@users.noreply.github.com>
2023-09-06 15:56:30 +0200

    Snack 2 completed

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -1,4 +1,6 @@
 ﻿// Snack 1: L’utente inserisce due numeri in successione. Il software stampa il maggiore.
+using System.Collections;
+using System;
 using System.Globalization;
 
 bool isNumber = false; // used to check if user insert a number
@@ -39,4 +41,32 @@ switch (compareNumbers)
         break;
 }
 
+// Snack 2 : L’utente inserisce due parole in successione. Il software stampa prima la parola più corta, poi la parola più lunga.
 
+// Array than contains the two string
+string?[] words = new string[2];
+
+// Get the first string
+Console.Write("Insert the first word: ");
+words[0] = Console.ReadLine();
+
+Console.WriteLine();
+
+// Get the second string
+Console.Write("Insert the second word: ");
+words[1] = Console.ReadLine();
+
+// Sort the array based on string lenght
+Array.Sort(words, (x, y) => x.Length.CompareTo(y.Length));
+
+// Print in console all the strings in words array
+foreach (string word in words)
+{
+    if (words.Last() == word)
+    {
+        Console.Write(word);
+        return;
+    }
+
+    Console.Write($"{word}, ");
+}
